830744a30796a9e3752c53fc1cc8b80e6e597280
ethanleep <20498042+ethanleep@users.noreply.github.com>
2019-02-11 21:19:29 -0600

    Added code for reading/writing the eeprom

---
+++ SFP_read_experimental_code/SFP_read_experimental_code.ino
@@ -0,0 +1,122 @@
+const int clockpin = 7;
+const int datapin = 6;
+int del = 250;
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(clockpin, OUTPUT);
+digitalWrite(clockpin, HIGH);
+pinMode(datapin, OUTPUT);
+digitalWrite(datapin, HIGH);
+delay(500);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+start();
+datahigh();
+cloc();
+datalow();
+cloc();
+datahigh();
+cloc();
+datalow();
+cloc();
+cloc();
+cloc();
+cloc();
+cloc();
+cloc();
+
+//First bit
+cloc();
+//Second bit
+cloc();
+//Third bit
+cloc();
+//Fourth bit
+cloc();
+//Fifth bit
+cloc();
+//Sixth bit
+cloc();
+//Seventh bit
+cloc();
+//Eight bit
+cloc();
+
+datalow();
+cloc();
+start();
+datahigh();
+cloc();
+datalow();
+cloc();
+datahigh();
+cloc();
+datalow();
+cloc();
+cloc();
+cloc();
+cloc();
+datahigh();
+cloc();
+datalow();
+cloc();
+rele();
+cloc();
+cloc();
+cloc();
+cloc();
+cloc();
+cloc();
+cloc();
+cloc();
+tak();
+cloc();
+delay(500000);
+}
+
+void cloc() {
+digitalWrite(clockpin, HIGH);
+delayMicroseconds(del);
+digitalWrite(clockpin, LOW);
+delayMicroseconds(del);
+}
+
+void datahigh() {
+digitalWrite(datapin, HIGH);
+delayMicroseconds(del);
+}
+
+void datalow() {
+  digitalWrite(datapin, LOW);
+delayMicroseconds(del);
+}
+
+void start() {
+  digitalWrite(datapin, HIGH);
+  delayMicroseconds(del);
+  digitalWrite(clockpin, HIGH);
+  delayMicroseconds(del);
+  digitalWrite(datapin, LOW);
+  delayMicroseconds(del);
+  digitalWrite(clockpin, LOW);
+  delayMicroseconds(del);
+}
+
+void rele() {
+  pinMode(datapin, INPUT);
+}
+
+void tak() {
+  pinMode(datapin, OUTPUT);
+  digitalWrite(datapin, LOW);
+}
+
+void stap() {
+  digitalWrite(clockpin, HIGH);
+  delayMicroseconds(del);
+  digitalWrite(datapin, HIGH);
+}
+
---
+++ total_SFP_eeprom_control/total_SFP_eeprom_control.ino
@@ -0,0 +1,115 @@
+
+int clockPin = 7;
+int dataPin = 6;
+byte memory = 0;
+
+void setup() {
+  //set pins to output because they are addressed in the main loop
+  pinMode(clockPin, OUTPUT);
+  digitalWrite(clockPin, HIGH);
+  pinMode(dataPin, OUTPUT);
+  digitalWrite(dataPin, LOW);
+  Serial.begin(115200);
+}
+
+void loop() {
+  start();
+  readData();
+  delay(1000);
+}
+void start() {
+  pinMode(dataPin, OUTPUT);
+  digitalWrite(dataPin, LOW);
+  delayMicroseconds(5);
+  digitalWrite(clockPin, LOW);
+  delayMicroseconds(5);
+}
+
+void writeData() {
+
+}
+
+void readData() {
+  shiftOut(dataPin, clockPin, MSBFIRST, 160);
+  pinMode(dataPin, INPUT);
+  delayMicroseconds(5);
+  digitalWrite(clockPin, HIGH);
+  delayMicroseconds(5);
+  if (digitalRead(dataPin) == 1) {
+    Serial.println("Module busy");
+  }
+  else {
+    digitalWrite(clockPin, LOW);
+    delayMicroseconds(5);
+    pinMode(dataPin, OUTPUT);
+    delayMicroseconds(5);
+    shiftOut(dataPin, clockPin, MSBFIRST, memory);
+    pinMode(dataPin, INPUT);
+    delayMicroseconds(5);
+    digitalWrite(clockPin, HIGH);
+    delayMicroseconds(5);
+    if (digitalRead(dataPin) == 1) {
+      Serial.println("Module busy");
+    }
+    else {
+      pinMode(dataPin, OUTPUT);
+      digitalWrite(clockPin, LOW);
+      delayMicroseconds(5);
+      digitalWrite(dataPin, HIGH);
+      delayMicroseconds(5);
+      digitalWrite(clockPin, HIGH);
+      delayMicroseconds(5);
+      digitalWrite(dataPin, LOW);
+      delayMicroseconds(5);
+      digitalWrite(clockPin, LOW);
+      shiftOut(dataPin, clockPin, MSBFIRST, 161);
+      pinMode(dataPin, INPUT);
+      delayMicroseconds(5);
+      digitalWrite(clockPin, HIGH);
+      delayMicroseconds(5);
+      if (digitalRead(dataPin) == 1) {
+        Serial.println("Module busy");
+      }
+      else {
+        digitalWrite(clockPin, LOW);
+        delayMicroseconds(5);
+        for (int idek = 0; idek < 255; idek++) {
+          byte j = shiftIn(dataPin, clockPin, MSBFIRST);
+          delay(1);
+          Serial.print("Address ");
+          Serial.print(idek);
+          Serial.print(" is ");
+          Serial.println(j);
+          delay(2);
+          if(idek == 245) {
+            break;
+          }
+          delayMicroseconds(5);
+          pinMode(dataPin, OUTPUT);
+          delayMicroseconds(5);
+          digitalWrite(dataPin, LOW);
+          delayMicroseconds(5);
+          digitalWrite(clockPin, HIGH);
+          delayMicroseconds(5);
+          digitalWrite(clockPin, LOW);
+          delayMicroseconds(5);
+          pinMode(dataPin, INPUT);
+        }
+        delayMicroseconds(5);
+        pinMode(dataPin, OUTPUT);
+        digitalWrite(dataPin, HIGH);
+        delayMicroseconds(5);
+        digitalWrite(clockPin, HIGH);
+        delayMicroseconds(5);
+        digitalWrite(clockPin,  LOW);
+        delayMicroseconds(5);
+        digitalWrite(dataPin, LOW);
+        delayMicroseconds(5);
+        digitalWrite(clockPin, HIGH);
+        delayMicroseconds(5);
+        digitalWrite(dataPin, HIGH);
+      }
+    }
+  }
+}
+
